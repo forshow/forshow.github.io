2b23ee99c95a2c9318bbb7ea7050fd3e6727bf74
Michael Feninets <carpinua@gmail.com>
2018-12-06 14:32:45 +0200

    corrected smooth scrolling to move to the top of the target

--- rideUp/js/main.js
+++ rideUp/js/main.js
@@ -8,6 +8,15 @@ $(document).ready(function() {
     let pIcon = $('#price-icon');
     let cIcon = $('#contact-icon');
     let prevWindowTop = 70;
+    var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+    if (window.location.hash && isChrome) {
+        setTimeout(function () {
+            var hash = window.location.hash;
+            window.location.hash = "";
+            window.location.hash = hash;
+        }, 300);
+    }
 
     function ready() {
         element.click(upMe)
